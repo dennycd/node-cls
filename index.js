f96f0de13f0a1f4b532b3438f354d09744eba37d
Denny C. Dai <dennycd@me.com>
2013-06-12 15:18:50 -0700

    avoid exposing Method class to public

--- index.js
+++ index.js
@@ -2,5 +2,4 @@ var Class = require('./lib/Class.js');
 var defineClass = require('./lib/defineClass.js');
 
 module.exports.Class = Class;
-module.exports.Method = Method;
 module.exports.defineClass = defineClass;
