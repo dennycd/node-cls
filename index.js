d79a57bd5b67266195f392153bc765d5a1a0a4d5
Denny C. Dai <dennycd@me.com>
2013-07-31 22:43:49 -0700

    exposing defineInterface function

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var Class = require('./lib/Class.js');
 var defineClass = require('./lib/defineClass.js');
+var defineInterface = require('./lib/defineInterface.js');
 
 module.exports.Class = Class;
 module.exports.defineClass = defineClass;
+module.exports.defineInterface = defineInterface;
